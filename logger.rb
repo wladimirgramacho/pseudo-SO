f15f9278c39185b003c6bcd353bc23adfb277270
Wladimir Gramacho <wladimirgramacho@gmail.com>
2019-11-11 10:13:34 -0300

    refact: move dispatcher logging to new class

---
+++ logger.rb
@@ -0,0 +1,14 @@
+class Logger
+  def dispatcher(process)
+    puts 'dispatcher =>'
+    puts "\t PID:\t\t\t #{process[:pid]}"
+    puts "\t offset:\t\t #{process[:offset]}"
+    puts "\t blocos em memória:\t #{process[:memory_blocks]}"
+    puts "\t prioridade:\t\t #{process[:priority]}"
+    puts "\t tempo de processador:\t #{process[:exec_time]}"
+    puts "\t impressoras:\t\t 0"
+    puts "\t scanners:\t\t #{process[:scanner]}"
+    puts "\t modems:\t\t #{process[:modem]}"
+    puts "\t drivers:\t\t 0"
+  end
+end
--- main.rb
+++ main.rb
@@ -1,18 +1,11 @@
 require_relative 'process_manager'
+require_relative 'logger'
 
 def main
   process_manager = ProcessManager.new
+  log = Logger.new
   process_manager.processes.each do |process|
-    puts 'dispatcher =>'
-    puts "\t PID:\t\t\t #{process[:pid]}"
-    puts "\t offset:\t\t #{process[:offset]}"
-    puts "\t blocos em memória:\t #{process[:memory_blocks]}"
-    puts "\t prioridade:\t\t #{process[:priority]}"
-    puts "\t tempo de processador:\t #{process[:exec_time]}"
-    puts "\t impressoras:\t\t 0"
-    puts "\t scanners:\t\t #{process[:scanner]}"
-    puts "\t modems:\t\t #{process[:modem]}"
-    puts "\t drivers:\t\t 0"
+    log.dispatcher(process)
   end
 end
 
