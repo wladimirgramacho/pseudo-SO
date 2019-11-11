925f15f7632a9ce308f57dcab6bf28e1c6e522c0
Wladimir Gramacho <wladimirgramacho@gmail.com>
2019-11-11 09:56:46 -0300

    feat: show process information

--- main.rb
+++ main.rb
@@ -2,7 +2,18 @@ require_relative 'process_manager'
 
 def main
   process_manager = ProcessManager.new
-  puts process_manager.processes
+  process_manager.processes.each do |process|
+    puts 'dispatcher =>'
+    puts "\t PID:\t\t\t #{process[:pid]}"
+    puts "\t offset:\t\t #{process[:offset]}"
+    puts "\t blocos em memória:\t #{process[:memory_blocks]}"
+    puts "\t prioridade:\t\t #{process[:priority]}"
+    puts "\t tempo de processador:\t #{process[:exec_time]}"
+    puts "\t impressoras:\t\t 0"
+    puts "\t scanners:\t\t #{process[:scanner]}"
+    puts "\t modems:\t\t #{process[:modem]}"
+    puts "\t drivers:\t\t 0"
+  end
 end
 
 main
